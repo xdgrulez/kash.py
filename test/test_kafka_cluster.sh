d59f263133de0f0c3c2722d36551ce711a2ff85a
Ralph Debusmann <ralph.debusmann@mgb.ch>
2023-09-15 14:01:40 +0200

    kafi stuff removed; kash.py goes to maintenance mode

--- test/test_kafka_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export KASHPY_HOME=".."
-if [ -z $1 ]
-then
-    coverage run -m unittest test_kafka_cluster.Test
-else
-    coverage run -m unittest test_kafka_cluster.Test.$1
-fi
-coverage html
--- test/test_kafka_restproxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export KASHPY_HOME=".."
-if [ -z $1 ]
-then
-    coverage run -m unittest test_kafka_restproxy.Test
-else
-    coverage run -m unittest test_kafka_restproxy.Test.$1
-fi
-coverage html
